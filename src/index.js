7dc9aae62dc69a92bbcabab9600d4844cb8a21bb
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-06 12:51:58 -0500

    :wrench: Update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import "./styles/global.scss";
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './styles/global.scss';
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
