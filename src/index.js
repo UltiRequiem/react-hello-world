94a9dc2c888e0b280665454074ca7e6cc66459ee
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-06 11:55:55 -0500

    :tada: Begin

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./styles/global.scss";
+
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+/**
+ * @param {string} dir
+ */
+const fm = (dir) => path.join(__dirname, dir);
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: fm("dist"),
+    filename: "main.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    contentBase: fm("dist"),
+    compress: true,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [{ loader: "html-loader" }],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename:'[name].css'
+    })
+  ],
+};
